99da2c232c9dc6b6877ecec523ceee527754d9c9
Rao S <sanilr@equinox.ftpn.ornl.gov>
2023-08-09 18:26:33 -0400

    fixed hard coding of variables in task launch to take spiral value

--- include/irisbackend.hpp
+++ include/irisbackend.hpp
@@ -468,7 +468,7 @@ float Executor::initAndLaunch(std::vector<void*>& args, std::vector<int> sizes, 
             }
             std::vector<size_t> grid{(size_t)kernel_params[i*6]*kernel_params[i*6+3], (size_t)kernel_params[i*6+1]*kernel_params[i*6+4], (size_t)kernel_params[i*6+2]*kernel_params[i*6+5]};
             std::vector<size_t> block{(size_t)kernel_params[i*6+3], (size_t)kernel_params[i*6+4], (size_t)kernel_params[i*6+5]};
-            iris_task_kernel(task, kernel_names.at(i).c_str(), 3, NULL, grid.data(), block.data(), 3+pointers, params.data(), params_info.data());
+            iris_task_kernel(task, kernel_names.at(i).c_str(), 3, NULL, grid.data(), block.data(), sig_types.size()+pointers, params.data(), params_info.data());
         } else{
             iris_task_kernel(task, kernel_names.at(i).c_str(), 1, NULL, &size, NULL, 3+pointers, params.data(), params_info.data());
         }
